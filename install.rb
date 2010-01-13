af9533cf3d1d96926940593ff6759df2d16e7fd3
dinedine <didier.lafforgue@gmail.com>
2010-01-13 22:13:30 +0100

    fix typo in README + change target folders when installing the plugin

--- install.rb
+++ install.rb
@@ -3,13 +3,13 @@ require 'fileutils'
 RAILS_ROOT = File.expand_path( File.join(File.dirname(__FILE__), '../../../') )
 
 ['images', 'stylesheets', 'javascripts'].each do |folder|
-  unless FileTest.exist? File.join(RAILS_ROOT, 'public', folder, 'super_finder')
-    FileUtils.mkdir( File.join(RAILS_ROOT, 'public', folder, 'super_finder') )
+  if folder == 'images'
+  unless FileTest.exist? File.join(RAILS_ROOT, 'public', 'images', 'super_finder')
+    FileUtils.mkdir( File.join(RAILS_ROOT, 'public', 'images', 'super_finder') )
   end
   
   FileUtils.cp(
     Dir[File.join(File.dirname(__FILE__), 'assets', folder, '*')],
-    File.join(RAILS_ROOT, 'public', folder, 'super_finder'),
-    :verbose => true
+    File.join(RAILS_ROOT, 'public', folder, folder == 'images' ? 'super_finder' : nil)
   )
 end
